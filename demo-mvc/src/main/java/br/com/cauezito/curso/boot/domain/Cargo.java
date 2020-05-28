f2a19100a8e94e779696a5764bfc52064a9c712e
Cauê Santos <caah.sfc@live.com>
2020-05-28 02:15:13 -0300

    S3.16 Criando as entidades Departamento e Cargo

---
+++ demo-mvc/src/main/java/br/com/cauezito/curso/boot/domain/Cargo.java
@@ -0,0 +1,38 @@
+package br.com.cauezito.curso.boot.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CARGOS")
+public class Cargo extends AbstractEntity<Long> {
+
+	@Column(nullable = false, unique = true, length = 60)
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_departamento_fk")
+	private Departamento departamento;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	
+	
+	
+}
